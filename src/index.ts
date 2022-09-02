38414fe9ac188c8a120615ec05365ffcd4703787
adnan007d <adnanmansuri54@gmail.com>
2022-09-02 23:55:49 +0530

    rate limiter

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import postRouter from "../routes/posts";
 import votesRouter from "../routes/votes";
 import { verifyOtp } from "../controllers/verify-otp";
 import { sendOtp } from "../controllers/send-otp";
+import RateLimit from "express-rate-limit";
 
 dotenv.config();
 
@@ -17,12 +18,20 @@ const MONGODB_URI = process.env.MONGODB_URI!;
 
 console.log(MONGODB_URI);
 
+const limiter = RateLimit({
+  windowMs: 1000 * 10,
+  max: 5,
+  message: {
+    message: "Too Many Requests, Try Again Later"
+  }
+})
+
 app.use(express.json());
 app.use(cors());
 app.use("/api/posts", postRouter);
 app.post("/api/verify-otp", verifyOtp);
 app.post("/api/send-otp", sendOtp);
-app.use("/api/votes", votesRouter);
+app.use("/api/votes", limiter, votesRouter);
 
 app.get("/", (req, res) => {
   return res.send("WooW MeoW");

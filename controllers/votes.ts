a514c1d70f0419f5a8cdba20b0f579a04b7b685b
adnan007d <adnanmansuri54@gmail.com>
2022-09-06 16:48:22 +0530

    captcha

--- controllers/votes.ts
+++ controllers/votes.ts
@@ -3,14 +3,29 @@ import posts from "../modals/posts";
 import { ValidationError } from "yup";
 import dotenv from "dotenv";
 import votes, { votesValidationSchema } from "../modals/votes";
+import axios from "axios";
 
 dotenv.config();
+
+const isHuman = async (token:string|undefined) => { 
+  const response = await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
+  );
+  return response.data.success
+}
+
 const addVote: Handler = async (req, res) => {
   const body = req.body;
-  const {id} = req.params;
+  const { id } = req.params;
+  console.log(req.body)
   try {
     await votesValidationSchema.validate(body, { abortEarly: false });
-    const post = await posts.findOne({_id: id, deleted: false});
+
+    if (!await isHuman(req.body.token)) {
+      return res.status(401).json({ message: "Verification Failed!" });
+    }
+    
+    const post = await posts.findOne({ _id: id, deleted: false });
     if (!post) {
       return res.status(400).json({ message: "post not found" });
     }
